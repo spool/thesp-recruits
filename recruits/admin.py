615f1b6e11a6ec5be093c71f6e84e6919822af7d
Griffith Rees <griff.rees@gmail.com>
2011-10-12 15:01:28 +0100

    Expand admin list_display

--- recruits/admin.py
+++ recruits/admin.py
@@ -1,4 +1,8 @@
 from recruits.models import Recruit
 from django.contrib import admin
+class RecruitAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'institution', 'college', 'acting',
+                        "production", "tech", "musician", "tickets",)
 
-admin.site.register(Recruit)
+admin.site.register(Recruit, RecruitAdmin)
+
